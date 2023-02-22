d47cc959c6b8a9d6089c0b17b7354bd277cb1a7b
HemantWD <hemantajaysharma@gmail.com>
2023-02-22 22:42:57 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img src="images/main-banner-1.jpg" alt="main banner" className='img-fluid rounded-3' />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad S13+ Pro</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                  <Link to="/">Buy Now</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
